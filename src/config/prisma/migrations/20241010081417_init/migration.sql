20c2416b03f69f30234e432718a60f01b14dc654
sangtd <sangtd@vitalify.asia>
2024-10-11 08:41:10 +0700

    update db structure

--- src/config/prisma/migrations/20241010081417_init/migration.sql
+++ src/config/prisma/migrations/20241010081417_init/migration.sql
@@ -2,6 +2,8 @@
 CREATE TABLE "Document" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
     "vector" vector,
 
     CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
